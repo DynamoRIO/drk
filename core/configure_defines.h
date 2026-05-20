1f362dc89e97f95fd730ca30bf5d955933ba972d
Bin Wang <biwa@google.com>
2026-05-20 00:30:27 +0000

    Copy added files in `core` from `drk` branch
    Commit was created with
    ```
    git checkout drk -- $(git diff --name-only --diff-filter=A 3a5b5d7b090e6b208bcfd71e097c0f9b8d2c5a25..drk -- core)
    ```
    where `3a5b5d7` was the last commit before Peter's DRK implementation
    commit.

---
+++ core/configure_defines.h
@@ -0,0 +1,6 @@
+#ifndef _CONFIGDEFS_
+#define _CONFIGDEFS_ 1
+
+#define DYNAMORIO_DEFINES "[custom defines manually hacked by peter]"
+
+#endif /* _CONFIGDEFS_ */
---
+++ core/kernel_linux/hypercall_host_module_unittest.c
@@ -0,0 +1,7 @@
+#include "hypercall_host_module.c"
+
+int
+main()
+{
+    return 0;
+}
