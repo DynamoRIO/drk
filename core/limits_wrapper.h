3906ea7fc417f401c01fdd4ddd11bc425998146a
Bin Wang <biwa@google.com>
2026-05-20 18:30:41 -0700

    i#20: Copy files from `drk` branch (#27)
    * Copy added files in `core` from `drk` branch
    
    Commit was created with
    ```
    git checkout drk -- $(git diff --name-only --diff-filter=A 3a5b5d7b090e6b208bcfd71e097c0f9b8d2c5a25..drk -- core)
    ```
    where `3a5b5d7` was the last commit before Peter's DRK implementation
    commit.
    
    * Copy CI workflow from `drk` branch
    
    Commit created with
    ```
    git show drk:.github/workflows/ci-x86.yml > .github/workflows/ci-drk.yml
    ```

---
+++ core/limits_wrapper.h
@@ -0,0 +1,31 @@
+#ifndef __LIMITS_H_
+#define __LIMITS_H_
+/* Copied from /usr/include/limits.h */
+/* We don't have #include_next.
+   Define ANSI <limits.h> for standard 32-bit words.  */
+
+/* These assume 8-bit `char's, 16-bit `short int's,
+   and 32-bit `int's and `long int's.  */
+
+#include <linux/kernel.h>
+
+/* Number of bits in a `char'.	*/
+#define CHAR_BIT 8
+
+/* Minimum and maximum values a `signed char' can hold.  */
+#define SCHAR_MIN (-128)
+#define SCHAR_MAX 127
+
+/* Maximum value an `unsigned char' can hold.  (Minimum is 0.)  */
+#define UCHAR_MAX 255
+
+/* Minimum and maximum values a `char' can hold.  */
+#ifdef __CHAR_UNSIGNED__
+#    define CHAR_MIN 0
+#    define CHAR_MAX UCHAR_MAX
+#else
+#    define CHAR_MIN SCHAR_MIN
+#    define CHAR_MAX SCHAR_MAX
+#endif
+
+#endif
